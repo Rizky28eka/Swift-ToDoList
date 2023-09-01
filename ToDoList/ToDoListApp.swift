7070dad6f541b4cac8d10cbe439c7f3767f64fd5
Rizky28eka <eka28rizky03haryadi04@gmail.com>
2023-09-01 18:06:05 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by RIZKY EKA HARYADI on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
